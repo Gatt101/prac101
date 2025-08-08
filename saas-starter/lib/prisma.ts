5aa10ac8b5960a203ce009ca2424ac2e81a5c162
Gatt101 <gauravpatilk11@gmail.com>
2025-08-08 20:04:04 +0530

    Update

---
+++ saas-starter/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+
+const PrismaClientSingleton = () => {
+    return new PrismaClient()
+}
+
+const globalForPrisma = global as unknown as { prisma : PrismaClient};
+
+ export const prisma = globalForPrisma.prisma || PrismaClientSingleton();
+
+ if (process.env.NODE_ENV!=='production') {
+    globalForPrisma.prisma = prisma;
+ }
+
+ export default prisma;
